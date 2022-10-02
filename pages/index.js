60af4e2acd0e4c2e3338ccae19316b226a9145b7
sayeef-rahman <sayeef.cse@gmial.com>
2022-10-02 13:01:08 +0600

    server side rendering implemented

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
+import fs from "fs/promises";
+import path from "path";
 
 const HomePage = (props) => {
-    const {products} = props;
-  console.log(props);
+  const [data, setData] = useState([]);
+  const { products } = props;
+  console.log(products);
   const router = useRouter();
   const handleViewClients = () => {
     router.push("/clients");
@@ -27,7 +31,7 @@ const HomePage = (props) => {
 
 // get static data with getStaticPorps()
 
-// export async function getStaticPorps() {
+// export async function getStaticProps() {
 //   const products = [
 //     { id: 1, title: "iPhone 9" },
 //     { id: 2, title: "iPhone 10" },
@@ -40,21 +44,52 @@ const HomePage = (props) => {
 //   };
 // }
 
-// get static data with getStaticPorps()
+// get data from dummyData.json file in data directory(folder) projects
 
 export async function getStaticProps() {
-  //   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  //   const posts = await res.json();
-  const products = [
-    { id: 1, title: "iPhone 9" },
-    { id: 2, title: "iPhone 10" },
-    { id: 3, title: "iPhone 11" },
-  ];
+  const filePath = path.join(process.cwd(), "data", "dummyData.json");
+  const jsonData = await fs.readFile(filePath);
+  const products = JSON.parse(jsonData);
+
   return {
-    props: {
-      products,
-    },
-  };
+    props:{
+      products
+    }
+  }
 }
+
+// get static data from api with getStaticProps()
+
+// export async function getStaticProps() {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   const posts = await res.json();
+
+//   const products = [
+//     { id: 1, title: "iPhone 9" },
+//     { id: 2, title: "iPhone 10" },
+//     { id: 3, title: "iPhone 11" },
+//   ];
+
+//   return {
+//     props: {
+//       posts,
+//       products,
+//     },
+//   };
+// }
+
+
+// get data from serverSide
+
+// export async function getServerSideProps(){
+//   const res = await fetch("https://dummyjson.com/products?skip=0&limit=20");
+//   const serverProducts = await res.json();
+
+//   return {
+//     props:{
+//       serverProducts
+//     }
+//   }
+// }
 
 export default HomePage;
