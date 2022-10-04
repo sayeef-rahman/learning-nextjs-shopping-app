12f0eed5b7781b0c46d166d0d62e5237dc29febc
sayeef-rahman <sayeef.cse@gmial.com>
2022-10-04 13:19:03 +0600

    all users data showed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Script from "next/script";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+          crossorigin="anonymous"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"
+      ></Script>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
 const fetcher = (url) =>
   fetch(url).then((res) => {
     if (res.ok) {
-        return res.json();
-    }else{
-        return res.status;
+      return res.json();
+    } else {
+      return res.status;
     }
   });
 
@@ -17,22 +17,41 @@ const AllUsers = () => {
     fetcher
   );
 
-  console.log("data: ", data);
-  console.log("error: ", error);
-
-  if (error) return "An error has occurred.";
-  if (!data) return "Loading...";
+  if (error) {
+    return "An error has occurred.";
+  } else if (!data) {
+    return "Loading...";
+  }
+  console.log(data);
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>All Users</h1>
-      <Link href={"/"}>
-        <button>Back To Home</button>
-      </Link>
-
-      <div style={{ marginTop: "25px" }}>{
-        
-      }</div>
-    </div>
+    <>
+      <div className="text-center container">
+        <h1>All Users</h1>
+        <Link href={"/"}>
+          <button>Back To Home</button>
+        </Link>
+      </div>
+      <div style={{ marginTop: "25px" }} className=" container mx-auto row">
+        {data?.map((user) => {
+          return (
+            <div
+              className="card col-lg-3 m-3"
+              style={{ width: "18rem" }}
+              key={user.id}
+            >
+              <div className="card-body">
+                <h5 className="card-title">{user.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{user.email}</h6>
+                <p className="card-text">{user.company.catchPhrase}</p>
+                <Link href={`/users/${user.id}`}>
+                  <button className="btn btn-primary">Details</button>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
